1fb8c0dce02a53de8d24ce41272b0bd1c46f0403
Norberto Jiménez <norberto.j90@gmail.com>
2020-09-07 20:20:29 -0500

    Agregado codigo a funcion voltear

--- main.js
+++ main.js
@@ -12,26 +12,50 @@ const carta11 = document.getElementById("carta11");
 const carta12 = document.getElementById("carta12");
 const reloj = document.getElementById("startBtn")
 
+const parent = document.querySelector(".tablero .row");
+for(var i = 0; i < parent.children.length; i++){
+  parent.appendChild(parent.children[Math.random() * i | 0]);
+}
+
 var activas = 0;
 
 function voltear() {
-    if (activas < 2 && !this.classList.contains("visible") && !this.classList.contains("pareja")) {
-        this.classList.add("visible");
+    if (activas < 2 && !this.classList.contains("par1") && !this.classList.contains("par2") && !this.classList.contains("pareja")) {
+        var check = document.querySelector(".par1");
+        if (check === null) {
+            this.classList.add("par1");
+            this.classList.add("pareja");
+        } else {
+            this.classList.add("par2");
+            this.classList.add("pareja");
+        }
         activas++;
         if (activas === 2) {
-            var par = document.getElementsByClassName('visible');
-            console.log(par);
-            //idea: con querrySelector buscar las visibles y darles una clase diferente luego comparar si dentro de esas clases hay una clase similar
-            //idea: cambiar cartas a clase con el nombre del pokemon
-            setTimeout(function(){  
-                for (let i = 0; i < activas; i++) {
-                    var seleccion = document.querySelector(".visible");
-                    seleccion.classList.remove("visible");
-                }
+            var cartapar1 = document.querySelector(".par1 .carta-frente");
+            var cartapara =Array.from(cartapar1.children)
+            console.log(cartapara);
+
+            var cartapar2 = document.querySelector(".par2 .carta-frente");
+            var cartaparb =Array.from(cartapar2.children)
+            console.log(cartaparb);
+
+            if (cartapara == cartaparb) {
+                document.getElementById("aviso").innerHTML = "¡Felicidades!"
                 activas = 0;
-            }, 1000);
-            
-            
+            } else {
+                setTimeout(function(){  
+                    for (let i = 0; i < activas; i++) {
+                        var seleccion = document.querySelector(".pareja");
+                        seleccion.classList.remove("pareja");
+                    }
+                    activas = 0;
+                }, 1000);
+            }
+            var removerpar = document.querySelector(".par1");
+            removerpar.classList.remove("par1");
+
+            var removerpar = document.querySelector(".par2");
+            removerpar.classList.remove("par2");
         }
     }
 }
@@ -64,15 +88,3 @@ carta10.addEventListener("click", voltear);
 carta11.addEventListener("click", voltear);
 carta12.addEventListener("click", voltear);
 reloj.addEventListener("click", tiempoAtras);
-
-var parent = document.querySelector(".tablero .row");
-for(var i = 0; i < parent.children.length; i++){
-  parent.appendChild(parent.children[Math.random() * i | 0]);
-}
-
-// var mazo = Array.from(parent.children);
-
-// for (let i = 0; i < mazo.length; i++) {
-//     mazo[i] = 'carta' + i;
-//     console.log(mazo[i]);
-// }
