bfc84b76b79634c94487f6590e48e49d7321c2c3
Norberto Jiménez <norberto.j90@gmail.com>
2020-09-09 21:52:22 -0500

    Agregadas funciones errores en tiempo corregidos

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+
 const reloj = document.getElementById("startBtn")
-
-const random = document.querySelector(".tablero .row");
-for(var i = 0; i < random.children.length; i++){
-  random.appendChild(random.children[Math.random() * i | 0]);
-}
-
-cartas = document.querySelectorAll(".carta");
-for (let i = 0; i < cartas.length; i++) {
-    cartas[i].addEventListener("click", voltear);
-}
+const reset = document.getElementById("resetBtn")
 
 var activas = 0;
+var segundos = 60;
+
+function barajar() {
+    let random = document.querySelector(".tablero .row");
+    for (var i = 0; i < random.children.length; i++) {
+        random.appendChild(random.children[Math.random() * i | 0]);
+    }
+}
+
+function jugando() {
+    cartas = document.querySelectorAll(".carta");
+        for (let i = 0; i < cartas.length; i++) {
+        cartas[i].addEventListener("click", voltear);
+}
+}
 
 function voltear() {
     if (activas < 2 && !this.classList.contains("par") && !this.classList.contains("pareja")) {
@@ -22,8 +29,8 @@ function voltear() {
 
 function checarPar() {
     if (activas === 2) {
-        var cartapar = document.querySelectorAll(".pareja");
-        if (cartapar[0].dataset.pokemon==cartapar[1].dataset.pokemon) {
+        let cartapar = document.querySelectorAll(".pareja");
+        if (cartapar[0].dataset.pokemon == cartapar[1].dataset.pokemon) {
             parEncontrado(cartapar);
         } else {
             regresarCartas();
@@ -35,14 +42,15 @@ function parEncontrado(a) {
     for (let i = 0; i < a.length; i++) {
         a[i].classList.add("par")
         a[i].classList.remove("pareja")
-        }
-        document.getElementById("aviso").innerHTML = "¡Felicidades capturaste un par!"
-        activas = 0;
+    }
+    document.getElementById("aviso").innerHTML = "¡Capturaste un par!"
+    activas = 0;
+    victoria();
 }
 
 function regresarCartas() {
     document.getElementById("aviso").innerHTML = "¡Los pokemon han huido!"
-    setTimeout(function(){  
+    setTimeout(function () {
         for (let i = 0; i < activas; i++) {
             var seleccion = document.querySelector(".pareja");
             seleccion.classList.remove("pareja");
@@ -52,18 +60,47 @@ function regresarCartas() {
 }
 
 function tiempoAtras() {
-    var counter = 90;
-    setInterval(function() {
-        counter--;
-        if (counter >= 0) {
-        span = document.getElementById("tiempo");
-        span.innerHTML = counter;
-        }
-        if (counter === 0) {
-            alert('Se terminó tu tiempo :(');
-            clearInterval(counter);
-        }
+    reset.addEventListener("click", reseteo)
+    reloj.removeEventListener("click", tiempoAtras);
+    setInterval(function () {
+        tempo()
     }, 1000);
+    barajar();
+    jugando();
+}
+
+function reseteo() {
+    clearInterval(segundos);
+    segundos = 61;
+    cartas = document.querySelectorAll(".carta");
+    for (let i = 0; i < cartas.length; i++) {
+        cartas[i].classList.remove("par");
+        cartas[i].classList.remove("pareja");
+    }
+    barajar();
+    jugando();
+}
+
+function tempo () {
+    segundos--;
+    if (segundos === 0) {
+        clearTimeout(segundos);
+        alert('Se terminó tu tiempo :(');
+    } 
+    if (segundos >= 0) {
+        span = document.getElementById("tiempo");
+        span.innerHTML = segundos;
+    }
+}
+
+function victoria () {
+    let capturados = document.querySelectorAll(".par");
+    if (capturados.length === 12) {
+        clearInterval(segundos);
+        segundos = -1;
+        document.getElementById("aviso").innerHTML = "¡Felicidades!"
+        alert('¡Los capturaste todos!');
+    }
 }
 
 reloj.addEventListener("click", tiempoAtras);
